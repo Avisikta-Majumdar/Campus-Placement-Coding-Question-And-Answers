4db628b43f15554b9885338590b77290c316efc0
Avisikta Majumdar <47344480+Avisikta-Majumdar@users.noreply.github.com>
2021-06-14 11:11:42 +0530

    Add files via upload

--- TCS NQT Coding Questions and Answers/7. Oddly Even.py
+++ TCS NQT Coding Questions and Answers/7. Oddly Even.py
@@ -10,7 +10,9 @@ Expected output:
 
 
 Explanation
-The Sum of odd position digits 4 and 6 is 10. The Sum of even position digits 5 and 7 is 12. The difference is 12-10=2.
+The Sum of odd position digits 4 and 6 is 10.
+The Sum of even position digits 5 and 7 is 12.
+The difference is 12-10=2.
 
 
 
@@ -20,3 +22,15 @@ Input #2:
 Output:
 1
 '''
+
+oddPositionSum , evenPositionSum = 0,0
+for i in range(int(input("Testcase:-"))):
+    number = input()
+    for index, value in enumerate(number):
+        if index&1:#Odd Position
+            oddPositionSum+= int(value)
+        else:
+            evenPositionSum+= int(value)
+    print(abs(oddPositionSum - evenPositionSum))
+        
+    
