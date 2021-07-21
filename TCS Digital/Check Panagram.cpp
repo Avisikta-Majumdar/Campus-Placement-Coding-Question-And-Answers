75cb22ff259b43d3e66ef7c96aeeaa6261894264
Avisikta Majumdar <47344480+Avisikta-Majumdar@users.noreply.github.com>
2021-07-21 08:51:21 +0530

    Add files via upload

--- TCS Digital/Check Panagram.cpp
+++ TCS Digital/Check Panagram.cpp
@@ -1,24 +1,25 @@
 #include<iostream>
 #include<bits/stdc++.h>
-
+#define MAX_ADDRESS_LEN 120 
 using namespace std;
 
 int main()
 {   
     string s;
-    cin>>s;
+    getline(cin,s);
     int array[128]={0};
     for(int i=0;i<s.length();++i){
         if(s[i]>='A' && s[i]<='Z')
         {
             s[i]+=32;
         }
+        //cout<<s[i]<<" ";
         array[s[i]]+=1;
     }
     
     for(int i=97;i<123;++i){
         if(!array[i]){
-            cout<<"Not panagram";
+            cout<<array[i]<<"Not panagram";
             return 0;
         }
     }
@@ -38,3 +39,4 @@ Logic
 6. make an another for loop from 97 to 122('a' = 97 & 'z'=122)
 7.if (!array[i]) true means array[i] is 0 which means from a to z 1 or more than 1 char is missing then print Not Panagram and return 0
 8. if for loop completed then print Panagram
+*/
