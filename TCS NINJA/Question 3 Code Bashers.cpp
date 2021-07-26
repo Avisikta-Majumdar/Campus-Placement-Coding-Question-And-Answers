54a5c422854ffd2a6fb35e2e452b6428ff69fb14
Avisikta Majumdar <47344480+Avisikta-Majumdar@users.noreply.github.com>
2021-07-26 21:03:29 +0530

    Add files via upload

--- TCS NINJA/Question 3 Code Bashers.cpp
+++ TCS NINJA/Question 3 Code Bashers.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main()
 {
     int n,a=0,b=1,s;
-   cin>>n;
-   cout<<a<<" "<<b<<" ";
-   for(int i=2;i<n;++i)
+    cin>>n;
+    if(n>5 &&n<=20){cout<<a<<" "<<b<<" ";
+    for(int i=2;i<n;++i)
    {
        s=a+b;
        a=b;
        b=s;
        cout<<s<<" ";
-   }
+   }    
+    }
+    else{
+         cout<<"INVALID INPUT";
+        }
 
     return 0;
 }
